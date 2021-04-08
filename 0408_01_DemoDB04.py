7141aee834a235f13c5e6959ac6bc7059ba385d2
Jiseon-Park <asdsun07@gmail.com>
2021-04-08 10:16:16 +0900

    DB예제

---
+++ 0408_01_DemoDB04.py
@@ -0,0 +1,12 @@
+# 0408_01_DemoDB04.py
+
+import sqlite3
+
+# 연결 객체 리턴
+con = sqlite3.connect("c:\\work2\\Demo10.db")
+
+# 커서 객체 리턴
+cur = con.cursor()
+cur.execute("select * from Emp;")
+for row in cur :
+    print(row)
